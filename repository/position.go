31133f73c67a70e349b716fedfe72f567b91ac2f
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2023-01-08 21:04:15 +0700

    answer repository position

--- repository/position.go
+++ repository/position.go
@@ -43,14 +43,20 @@ func (p *positionRepository) UpdateByID(ctx context.Context, id int, position *m
 }
 
 func (p *positionRepository) Delete(ctx context.Context, id int) error {
-
-	// TODO: Buat fungsi untuk mengapus posisi
-	panic("impelement me")
+	// TODO: Buat fungsi untuk mengapus posisi ✔️
+	return p.Cfg.Database().WithContext(ctx).
+		Delete(&model.Position{}, id).Error
 
 }
 
 func (p *positionRepository) Fetch(ctx context.Context, limit, offset int) ([]*model.Position, error) {
-
 	// TODO: Buat fungsi untuk mendapatkan data position berdasarkan parameter
-	panic("impelement me")
+	var data []*model.Position
+	if err := p.Cfg.Database().WithContext(ctx).
+		Limit(limit).
+		Offset(offset).
+		Find(&data).Error; err != nil {
+		return nil, err
+	}
+	return data, nil
 }
